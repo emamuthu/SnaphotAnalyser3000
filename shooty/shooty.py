dcf4407be447e1697f4afa76dc34e547123d7dca
unknown <emamuthu@gmail.com>
2019-11-27 13:33:15 -0500

    commited with filter by project

--- shooty/shooty.py
+++ shooty/shooty.py
@@ -5,14 +5,25 @@ session=boto3.Session(profile_name='shooty')
 ec2=session.resource('ec2')
 
 @click.command()
-def list_instances():
+@click.option('--project',default=None,
+       help="only instance for project (tag Project :<name>)")
+
+def list_instances(project):
    "List Ec2 instances"
-   for i in ec2.instances.all():
-         print(','.join((i.id,
-                   i.instance_type,
-                   i.placement['AvailabilityZone'],
-                   i.state['Name'],
-                   i.public_dns_name)))
-   return
+   instance=[]
+
+   if project:
+       filters = [{'Name':'tag:project','Values':[project]}]
+       instances = ec2.instances.filter(Filters=filters)
+   else:
+       instances=ec2.instances.all()
+
+       for i in instances:
+           print(','.join((i.id,
+           i.instance_type,
+           i.placement['AvailabilityZone'],
+           i.state['Name'],
+           i.public_dns_name)))
+           return
 if __name__ == '__main__':
    list_instances()
